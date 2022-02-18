58855f5bc05f272f3fee6a77de86dbd8b72705fd
ElperdonXV <davidemartinografico@gmail.com>
2022-02-18 14:49:53 +0100

    ok migrations done success

---
+++ database/migrations/2022_02_18_133610_create_trains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 100);
+            $table->string('departure_station', 200);
+            $table->string('arrival_station', 200);
+            $table->decimal('carriages', 30);
+            $table->date('departures_time');
+            $table->date('arrival_time');
+            $table->boolean('in_time');
+            $table->boolean('cancelled');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
