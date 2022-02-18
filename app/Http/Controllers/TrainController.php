764563f0832ae793a9e9ff223672b3c85818f2d9
ElperdonXV <davidemartinografico@gmail.com>
2022-02-18 15:29:20 +0100

    time error

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Carbon\Carbon;
 use App\Train;
 
 class TrainController extends Controller
 {
     public function index(){
-        $todaydate = Carbon::now();
+        $todaydate = date("Y-m-d H:i:s");
         $trains = Train::where('departures_time', '==', $todayDate)->get();
         $data = [
             'trains' => $trains
