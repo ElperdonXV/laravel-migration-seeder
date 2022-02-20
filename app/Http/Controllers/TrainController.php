87c02017c7769762c8b444df6dadea0d2b22dfd8
ElperdonXV <davidemartinografico@gmail.com>
2022-02-20 18:27:49 +0100

    all done

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Train;
+use Carbon\Carbon;
 
 class TrainController extends Controller
 {
     public function index(){
-        $todaydate = date("Y-m-d H:i:s");
-        $trains = Train::where('departures_time', '==', $todayDate)->get();
+        $trains = Train::where('departures_time', '>=' , Carbon::today()->toDateString())->get();
         $data = [
             'trains' => $trains
         ];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,15 @@
 @endsection
 
 @section('content')
-    @dd($trains)
+    @foreach ($trains as $train)
+    <div>
+        <h1>Train id: {{ $train->id }}</h1>
+        <h2>Brand: {{$train->agency}}</h2>
+        <h3>Departure station: {{$train->departure_station}}</h3>
+        <h3>Departure time: {{$train->departures_time}}</h3>
+        <h3>Arrival station: {{$train->arrival_station}}</h3>
+        <h3>Arrival time: {{$train->arrival_time}}</h3>
+        <span>Number of carriages: {{$train->carriages}}</span>
+    </div>
+    @endforeach
 @endsection
