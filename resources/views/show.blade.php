3cda6da7bbe5088523fb0137eee61d68707e4c26
ElperdonXV <davidemartinografico@gmail.com>
2022-02-21 17:27:21 +0100

    work done

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,8 +7,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            @foreach ($trains as $train)
-            <div class="card col-2 m-2">
+            <div class="card col-4">
                 <h1>Train id: {{ $train->id }}</h1>
                 <h2>Brand: {{$train->agency}}</h2>
                 <h3>Departure station: {{$train->departure_station}}</h3>
@@ -16,9 +15,7 @@
                 <h3>Arrival station: {{$train->arrival_station}}</h3>
                 <h3>Arrival time: {{$train->arrival_time}}</h3>
                 <span>Number of carriages: {{$train->carriages}}</span>
-                <a href="{{route('show', $train)}}" class="btn btn-primary">View Train</a>
             </div>
-            @endforeach
         </div>
     </div>
 @endsection
